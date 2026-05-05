ff2d566ff92ef9a321880b00d3a9f2c40700c32f
s1nh <hbyls@live.com>
2026-05-05 17:31:09 +0800

    add native Qwen3.5 model with paged KV cache support (self-attn native, GatedDeltaNet HF)

--- nanovllm/models/__init__.py
+++ nanovllm/models/__init__.py
@@ -2,15 +2,22 @@ from transformers import AutoConfig
 
 from nanovllm.models.qwen2 import Qwen2ForCausalLM
 from nanovllm.models.qwen3 import Qwen3ForCausalLM
-from nanovllm.models.qwen3_5 import Qwen35ForCausalLM
+from nanovllm.models.qwen3_5 import Qwen35ForCausalLM as Qwen35HFForCausalLM
+from nanovllm.models.qwen3_5_native import Qwen35ForCausalLM as Qwen35NativeForCausalLM
 
 
 SUPPORTED_MODEL_CLS = {
     "qwen2": Qwen2ForCausalLM,
     "qwen3": Qwen3ForCausalLM,
-    "qwen3_5": Qwen35ForCausalLM,
-    "qwen3_5_text": Qwen35ForCausalLM,
+    "qwen3_5": Qwen35HFForCausalLM,
+    "qwen3_5_text": Qwen35HFForCausalLM,
 }
+
+# Override with native version if requested
+import os as _os
+if _os.environ.get("NANOVLLM_QWEN35_NATIVE", "") == "1":
+    SUPPORTED_MODEL_CLS["qwen3_5"] = Qwen35NativeForCausalLM
+    SUPPORTED_MODEL_CLS["qwen3_5_text"] = Qwen35NativeForCausalLM
 
 
 def get_model_cls(hf_config: AutoConfig):
